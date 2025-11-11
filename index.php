4bce824713a96a316fe74d5be3dd1e6d2949007b
Kylewercoir <Kylewercoir@gmail.com>
2025-11-10 21:13:29 -0500

    START OF INDEX PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once   '/includes/config.php';
+require_once   '/includes/Database.php';
+require_once  '/includes/Inventory.php';
+$config = require  '/includes/config.php';
+$inventory = new Inventory($config);
+include  '/includes/header.php';
+?>
+<section class="py-4">
+  <div class="row align-items-center">
+    <div class="col-md-7">
+      <h2>Welcome to the Inventory Management System</h2>
+      <p>View products, create an account, or sign in to manage inventory.</p>
+      <a href="/products.php" class="btn btn-primary">Browse Products</a>
+    </div>
+    <div class="col-md-5">
+      <img src="/hero.png" class="img-fluid" alt="Inventory illustration">
+    </div>
+  </div>
+</section>
+
+<?php include  '/includes/footer.php'; ?>
