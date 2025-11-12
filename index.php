10b11da55614b65fe0dce0c85a890366df9e6121
Kylewercoir <Kylewercoir@gmail.com>
2025-11-11 22:49:58 -0500

    updates index

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once   '/includes/config.php';
 require_once   '/includes/Database.php';
 require_once  '/includes/Inventory.php';
+require_once 'styles.css';
 $config = require  '/includes/config.php';
 $inventory = new Inventory($config);
 include  '/includes/header.php';
@@ -9,7 +10,7 @@ include  '/includes/header.php';
 <section class="py-4">
   <div class="row align-items-center">
     <div class="col-md-7">
-      <h2>Welcome to Kyles Final PHP Project Inventory Management System</h2>
+      <h2>Welcome to Kyles Final PHP Project Inventory Management System for Kylewercoiergaming merch</h2>
       <p>View products, create an account, or sign in to manage inventory.</p>
       <a href="/products.php" class="btn btn-primary">Browse Products</a>
     </div>
